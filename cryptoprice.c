071c82a08d9c0370b71850f3789fe721fd768e55
bowdens <tomfbowden12@gmail.com>
2018-02-04 22:52:34 +1100

    fixed several features

--- cryptoprice.c
+++ cryptoprice.c
@@ -2,24 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <argp.h>
+#include <time.h>
 #include <curl/curl.h>
 
 #define PRICELEN 8
 
-char *settings_path = ".cryptoprice_settings";
+const char *settings_path = ".cryptoprice_settings";
+const char *price_path = ".cryptoprice_prices";
 
 char *currency = "USD";
 char *currency_lower = "usd";
 char *defaultcoin = "bitcoin";
 
 int writemode = 0;
-const char *writemodes[] = { "humanreadable", "simple" };
-const int maxwritemode = 1;
+const char *writemodes[] = { "humanreadable", "simple", "change", "24hchange" };
+const int maxwritemode = 3;
 
 const char *argp_program_bug_address = "@bowdens [github]";
 const char *argp_program_version = "cryptoprice 0.1";
 
 int parsed_args = 0;
+
+int read_settings(const char*);
+int write_settings(const char *);
+
+typedef struct _LastPrice {
+    char name[1000];
+    double price;
+    time_t date;
+} LastPrice;
+
+LastPrice *read_price(char*, char*);
+int write_price(char *coin, char *curr, double price, int date);
 
 void copy_to_lower(char *str, char *tocopy) {
     free(str);
@@ -29,10 +43,6 @@ void copy_to_lower(char *str, char *tocopy) {
         str[i] = tolower(tocopy[i]);
     }
 }
-
-
-int read_settings(char*);
-int write_settings(char *);
 
 struct MemoryStruct {
     char *memory;
@@ -105,26 +115,91 @@ int print_price(char *json, char *coin){
         }
     }
 
-    //printf("pos = \"%s\"\n",pos);
-
     double price = -1;
     if(sscanf(price_str, "%lf", &price) == 0){
-        printf("could not find price in sscanf\n");
+        fprintf(stderr,"Warning: could not find price in json\n");
         free(price_str);
         free(symb);
         return 1;
     }
 
+    time_t lastdate = 0;
+    char needle3[128] = "\"last_updated\": \"";
+    char *pos3 = strstr(json, needle3);
+    if(pos3 != NULL) {
+        pos3 += strlen(needle3);
+        char *tempdate = strdup(pos3);
+        int found = 0;
+        for(int i = 0; i < strlen(tempdate); i++){
+            if(tempdate[i] == '"'){
+                tempdate[i] = '\0';
+                found = 1;
+                break;
+            }
+        }
+        if(found == 0){
+            free(tempdate);
+            lastdate = time(NULL);
+        }else{
+            if(sscanf(tempdate, "%ld", &lastdate) != 1){
+                fprintf(stderr,"Warning: Could not find last_updated in json\n");
+                free(tempdate);
+                lastdate = time(NULL);
+            }
+        }
+    }
+
+    double change24h = 0;
+    char needle4[128] = "\"percent_change_24h\": \"";
+    char *pos4 = strstr(json, needle4);
+    if(pos4 != NULL) {
+        pos4 += strlen(needle4);
+        if(sscanf(pos4, "%lf", &change24h) != 1){
+            fprintf(stderr, "Warning: Could not find 24h change in json\n");
+        }
+    }
+
+    char strdatenow[256];
+    struct tm *datenow = localtime(&lastdate);
+    strftime(strdatenow, 256, "%c", datenow);
+
     switch(writemode){
         case 0:
             //human
-            printf("1 %s = %.2lf %s\n",symb, price, found_curr?currency:"USD");
+            printf("1 %s = %.2lf %s, as of %s.\n",symb, price, found_curr?currency:"USD",strdatenow);
             break;
         case 1:
             //simple
             printf("%lf\n",price);
             break;
+        case 2:
+            //change
+            {
+            printf("1 %s = %.2lf %s, as of %s.", symb, price, found_curr?currency:"USD", strdatenow);
+
+            LastPrice *lp = NULL;
+            lp = read_price(symb, found_curr?currency:"USD");
+            if(lp){
+                double change = price/lp->price * 100.0 - 100;
+                if(change < 0) change = -change;
+                char strdate[256];
+                struct tm *date = localtime(&(lp->date));
+                strftime(strdate, 256, "%c", date);
+                printf(" %s by %.2lf%% since you last checked on %s\n",
+                    lp->price >= price ? "Increased":"Decreased", change, strdate);
+            }else{
+                printf("\n");
+            }
+            }
+            break;
+        case 3:
+            //24h change
+            printf("1 %s = %.4lf %s %s%.2lf%%\n",symb, price, currency, change24h>=0?" +":" ", change24h);
+            break;
     }
+
+    write_price(symb, found_curr?currency:"USD", price, lastdate);
+
     return 0;
 }
 
@@ -229,6 +304,7 @@ static int parse_opt(int key, char *arg, struct argp_state *state) {
             parsed_args++;
             break;
         case 777:
+            //set currency
             free(currency);
             currency = strdup(arg);
             for(int i = 0; i < strlen(currency); i ++){
@@ -236,17 +312,20 @@ static int parse_opt(int key, char *arg, struct argp_state *state) {
             }
             copy_to_lower(currency_lower, currency);
             write_settings(settings_path);
-            parsed_args++;
+            //parsed_args++;
             break;
         case 888:
+            //check currency
             printf("%s\n",currency);
             parsed_args++;
             break;
         case 999:
+            //set writemode
             set_writemode(arg);
-            parsed_args++;
+            //parsed_args++;
             break;
         case 1000:
+            //check writemode
             if(writemode >= 0 && writemode <= maxwritemode){
                 printf("Writing in %s mode\n", writemodes[writemode]);
             } else {
@@ -257,13 +336,20 @@ static int parse_opt(int key, char *arg, struct argp_state *state) {
             parsed_args++;
             break;
         case 1001:
+            //set default coin
             free(defaultcoin);
             defaultcoin = strdup(arg);
             write_settings(settings_path);
-            parsed_args++;
+            //parsed_args++;
             break;
         case 1002:
+            //check default coin
             printf("%s\n",defaultcoin);
+            parsed_args++;
+            break;
+        case 1003:
+            //purge saved prices
+            remove(price_path);
             parsed_args++;
             break;
         case ARGP_KEY_ARG:
@@ -273,24 +359,25 @@ static int parse_opt(int key, char *arg, struct argp_state *state) {
         case ARGP_KEY_NO_ARGS:
             if(parsed_args==0) get_price(defaultcoin,currency);
             break;
-    };
+    }
     return 0;
 }
 
 struct argp_option options[] = {
-    { 0, 0, 0, 0, "price options" },
+    { 0, 0, 0, 0, "Prices" },
     { "eth", 'e', 0, 0, "check price of ethereum" },
     { "btc", 'b', 0, 0, "check price of bitcoin" },
     { "ltc", 'l', 0, 0, "check price of litecoin" },
     { "iota", 'i', 0, 0, "check price of iota" },
     { "coin", 'o', "COIN", 0, "check the price of another coin" },
-    { 0, 0, 0, 0, "other options" },
+    { 0, 0, 0, 0, "Settings" },
     { "currency", 777, "CURRENCY", 0, "change the default currency" },
     { "cc", 888, 0, 0, "check the current currency" },
     { "writemode", 999, "WRITEMODE", 0, "change the default writemode" },
     { "cwm", 1000, 0, 0, "check the current writemode" },
     { "defaultcoin", 1001, "COIN", 0, "change the default coin (displayed when no arguments are given)" },
     { "cdc", 1002, 0, 0, "check the default coin" },
+    { "purgeprices", 1003, 0, 0, "deletes all saved historical prices" },
     { 0 }
 };
 
@@ -311,7 +398,7 @@ int main(int argc, char **argv) {
     }
 }
 
-int read_settings(char *path) {
+int read_settings(const char *path) {
     FILE *fp = fopen(path, "r");
     if(fp == NULL) {
         write_settings(path);
@@ -364,7 +451,7 @@ int read_settings(char *path) {
     fclose(fp);
 }
 
-int write_settings(char *path) {
+int write_settings(const char *path) {
     FILE *fp = fopen(path, "w");
     if(fp == NULL) return 1;
 
@@ -372,4 +459,145 @@ int write_settings(char *path) {
     fprintf(fp, "WRITEMODE:\n%d\n",writemode);
     fprintf(fp, "DEFAULTCOIN:\n%s\n",defaultcoin);
     fclose(fp);
+}
+
+void price_warning(){
+    fprintf(stderr, "Warning: '%s' last price file is incorrectly formatted\n",price_path);
+}
+
+LastPrice *read_price(char *coin, char *curr){
+    FILE *fp = fopen(price_path, "r");
+    if(fp == NULL){
+        //fprintf(stderr, "Warning: Could not find '%s', so attempting to create file\n",price_path);
+        fp = fopen(price_path, "w");
+        if(fp == NULL){
+            fprintf(stderr, "Warning: Could not create file '%s'\n",price_path);
+            return NULL;
+        }
+        fclose(fp);
+        fopen(price_path, "r");
+        if(fp == NULL){
+            fprintf(stderr, "Warning: Could create file '%s' but could not open it\n",price_path);
+            return NULL;
+        }
+    }
+
+    char line[1000];
+
+    while(fgets(line, 1000, fp) != NULL){
+        int size = strlen(line);
+        for(int i = size-1; i >= 0; i--){
+            if(line[i] == '\n'){
+                line[i] = '\0';
+                break;
+            }
+        }
+        if(strcmp(line, coin) == 0){
+            if(fgets(line, 1000, fp) != NULL){
+                int size2 = strlen(line);
+                for(int i = size-1; i >= 0; i--){
+                    if(line[i] == '\n'){
+                        line[i] = '\0';
+                        break;
+                    }
+                }
+                if(strcmp(line, curr) == 0){
+
+                    double price = -1;
+                    int date = 0;
+                    if(fgets(line, 1000, fp) == NULL){
+                        price_warning();
+                        fclose(fp);
+                        return NULL;
+                    }
+                    if(sscanf(line, "%lf", &price) != 1){
+                        price_warning();
+                        fclose(fp);
+                        return NULL;
+                    }
+                    if(fgets(line, 1000, fp) == NULL) {
+                        price_warning();
+                        fclose(fp);
+                        return NULL;
+                    }
+                    if(sscanf(line, "%d", &date) != 1) {
+                        price_warning();
+                        fclose(fp);
+                        return NULL;
+                    }
+                    LastPrice *lp = malloc(sizeof(LastPrice));
+                    strncpy(lp->name, coin, 1000);
+                    lp->price = price;
+                    lp->date = date;
+
+                    fclose(fp);
+
+                    return lp;
+                }
+            } else {
+                price_warning();
+                fclose(fp);
+                return NULL;
+            }
+        }
+    }
+    fclose(fp);
+    //fprintf(stderr, "Could not find matching coin in price history\n");
+    return NULL;
+}
+
+int write_price(char *coin, char *curr, double price, int date) {
+    FILE *fp = fopen(price_path, "r");
+    if(fp == NULL){
+        fp = fopen(price_path, "w");
+        if(fp == NULL) return 1;
+        fprintf(fp,"%s\n", coin);
+        fprintf(fp,"%s\n", curr);
+        fprintf(fp,"%lf\n", price);
+        fprintf(fp,"%d\n", date);
+        fclose(fp);
+        return 0;
+    }
+
+    char *tempfile = strdup(price_path);
+    strcat(tempfile, ".tmp");
+    FILE *temp = fopen(tempfile, "w");
+
+    char *nlcoin = strdup(coin);
+    strcat(nlcoin, "\n");
+    char *nlcurr = strdup(curr);
+    strcat(nlcurr, "\n");
+
+    char line[1000];
+    char line2[1000];
+    while(fgets(line, 1000, fp) != NULL){
+        if(strcmp(line, nlcoin) == 0){
+            if(fgets(line2, 1000, fp) != NULL) {
+                if(strcmp(line2, nlcurr) == 0){
+                    fgets(line, 1000, fp); //price
+                    fgets(line, 1000, fp); //date
+                } else {
+                    fprintf(temp, "%s%s", line, line2);
+                }
+            }
+        } else {
+            fprintf(temp, "%s",line);
+        }
+    }
+    fclose(fp);
+    remove(price_path);
+    free(nlcoin);
+    free(nlcurr);
+
+    fprintf(temp, "%s\n%s\n%lf\n%d\n",coin,curr, price, date);
+    fclose(temp);
+
+    int res = rename(tempfile, price_path);
+    free(tempfile);
+
+    if(res != 0){
+        printf("Warning: Could not overwrite old price file\n");
+        return 1;
+    }
+    return 0;
 }
